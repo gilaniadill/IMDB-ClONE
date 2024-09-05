de05ffa4a4428f495d4535b8e099d9b5a9c91c0e
Adil Gilani <adilgilani94@gmail.com>
2024-09-05 07:49:43 -0700

    add about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+        <div className="max-w-6xl 
+        mx-auto 
+        p-3 
+        space-y-4">
+            <h1 className="text-2xl 
+            font-medium
+            text-amber-600">About</h1>
+            <p>
+                 Welcome to IMDB, your ultimate destination for all things cinema! 
+                 At IMDB, we're passionate about bringing the world of movies right 
+                 to your fingertips. Whether you're a casual viewer, a dedicated film buff,
+                  or someone just looking for the next great watch, our extensive database 
+                  and user-friendly interface are here to help you discover, explore, and 
+                  enjoy your favorite films and shows. With a vast collection of movie titles, 
+                  detailed reviews, and insightful recommendations, IMDB is designed to 
+                   cater to all your entertainment needs.
+            </p>
+            <p>
+             Our team of film enthusiasts and industry experts works tirelessly to ensure that 
+             IMDB remains at the forefront of movie information and trends. We pride 
+             ourselves on providing accurate and up-to-date content, including cast and crew 
+             details, plot summaries, and user ratings. Our goal is to create a dynamic and 
+             engaging platform where users can connect with the films they love, share their 
+             opinions, and find inspiration for their next viewing adventure. Join us at 
+             IMDB and immerse yourself in the captivating world of movies like never before!
+            </p>
+        </div>
+    </>
+  )
+}
+
+export default About
