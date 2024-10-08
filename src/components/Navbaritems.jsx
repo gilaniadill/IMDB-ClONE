109dd1601cbb0c5814c116748afb18028ab1f9ad
Adil Gilani <adilgilani94@gmail.com>
2024-10-08 17:53:17 +0500

    add card component

--- src/components/Navbaritems.jsx
+++ src/components/Navbaritems.jsx
@@ -12,7 +12,7 @@ const Navbaritems = ({title, param}) => {
   return (
     <>
         <Link className={`hover:text-amber-600
-          font-semibold
+          font-normal
           ${genre===param?
           'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg':''}`} 
           href={`/?genre=${param}`}>{title}</Link>
